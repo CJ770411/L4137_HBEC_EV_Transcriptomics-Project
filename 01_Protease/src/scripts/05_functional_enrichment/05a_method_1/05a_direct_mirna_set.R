57cbf0c40135bac731de35d7e939dfaee6022538
Christopher Janschke <chris.janschke@hotmail.co.uk>
2026-05-28 17:11:52 +0100

    ADDING TODAY'S WORK - 28.05.2026

--- 01_Protease/src/scripts/05_functional_enrichment/05a_method_1/05a_direct_mirna_set.R
+++ 01_Protease/src/scripts/05_functional_enrichment/05a_method_1/05a_direct_mirna_set.R
@@ -1,31 +1,38 @@
 library(readr)
+library(dplyr)
 
 # Read in results from '04b_DE_analysis_edgeR'
 dose24_sig_data <- read_csv("/Users/christopherjanschke/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/04_DE_analysis/04b_DE_analysis_edgeR/sig_results_FDR005_dose24.csv")
 count_matrix <- read_csv("~/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/04_DE_analysis/04a_create_count_matrix/deseq2_count_matrix.csv")
 
-
 # Remove rows with 0 counts
-reads <- count_matrix[rowSums(count_matrix[,-1]) > 0, ] # excludes miRNA column (col1) because it doesn't work on character vectors
+count_matrix_trimmed <- count_matrix[rowSums(count_matrix[,-1]) > 0, ] # excludes miRNA column (col1) because it doesn't work on character vectors
+
+# Set miRNA symbol column name for clarity
+colnames(count_matrix_trimmed)[1] <- "Symbol"
+
+# Sort significant miRNAs based on logFC
+dose24_sig_mirna_ranked_lfc_all <- dose24_sig_data %>% dplyr::arrange(desc(abs(logFC)))
+
+# Extract UP- and DOWN-regulated miRNAs
+dose24_sig_mirna_ranked_lfc_up <- dose24_sig_mirna_ranked_lfc_all %>% filter(logFC > 0) # Upregulated
+dose24_sig_mirna_ranked_lfc_down <- dose24_sig_mirna_ranked_lfc_all %>% filter(logFC < 0) # Downregulated
 
 
-# [start from here]
+# Create miRNA lists
+dose24_sig_mirnas_ranked_all <- dose24_sig_mirna_ranked_lfc_all$Symbol # All significant miRNAs
+dose24_sig_mirnas_ranked_up <- dose24_sig_mirna_ranked_lfc_up$Symbol # Upregulated miRNAs
+dose24_sig_mirnas_ranked_down <- dose24_sig_mirna_ranked_lfc_down$Symbol # Downregulated miRNAs
+dose24_background_mirnas <- count_matrix_trimmed$Symbol # All miRNAs with at least one read in one sample
+
+# Save miRNA lists as TXT file
+write(dose24_sig_mirnas_ranked_all, "~/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/05_functional_enrichment/05a_method_1/dose24_sig_mirnas_ranked_all.txt")
+write(dose24_sig_mirnas_ranked_up, "~/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/05_functional_enrichment/05a_method_1/dose24_sig_mirnas_ranked_up.txt")
+write(dose24_sig_mirnas_ranked_down, "~/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/05_functional_enrichment/05a_method_1/dose24_sig_mirnas_ranked_down.txt")
+write(dose24_background_mirnas, "~/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/05_functional_enrichment/05a_method_1/dose24_background_mirnas.txt")
 
 
 
-
-### Generate miRNA lists ###
-dose24_sig_data_ordered_lfc <- dose24_sig_data %>% dplyr::arrange(desc(abs(logFC)))
-dose24_all_data_ordered_lfc <- dose24_all_data %>% dplyr::arrange(desc(abs(logFC)))
-
-## Upregulated
-
-miRNA_ranked_list_dose24_sig <- dose24_sig_data_ordered_lfc$Symbol
-miRNA_ranked_list_dose24_all <- dose24_all_data_ordered_lfc$Symbol
-
-
-write(miRNA_ranked_list_dose24_sig, "~/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/05_functional_enrichment/05a_method_1/miRNA_ranked_list_dose24_sig.txt")
-write(miRNA_ranked_list_dose24_all, "~/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/05_functional_enrichment/05a_method_1/miRNA_ranked_list_dose24_all.txt")
 
 
 
---
+++ 01_Protease/src/scripts/05_functional_enrichment/05b_method_2/05b_target_gene.R
@@ -0,0 +1,70 @@
+# Load necessary libraries
+library(readr)
+library(multiMiR)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+
+
+# Read in results from '04b_DE_analysis_edgeR'
+dose24_data <- read_csv("/Users/christopherjanschke/GIT/L4137_HBEC_EV_Transcriptomics-Project/01_Protease/results/methods_sections/04_DE_analysis/04b_DE_analysis_edgeR/sig_results_FDR005_dose24.csv")
+
+# Extract UP- and DOWN-regulated miRNAs
+dose24_data_up <- dose24_data %>% filter(logFC > 0) # Upregulated
+dose24_data_down <- dose24_data %>% filter(logFC < 0) # Downregulated
+
+# Create list of miRNA IDs
+dose24_miRNA_list_up <- dose24_data_up$Symbol
+dose24_miRNA_list_down <- dose24_data_down$Symbol
+
+# Identify gene targets for each miRNA
+target_results_up <- get_multimir(mirna = dose24_miRNA_list_up, table = "validated")
+target_results_down <- get_multimir(mirna = dose24_miRNA_list_down, table = "validated")
+
+# Extract target gene symbols (for GO enrichment compatibility)
+genes_symbol_up <- unique(target_results_up@data$target_symbol)
+genes_symbol_down <- unique(target_results_down@data$target_symbol)
+
+# Perform GO enrichment analysis 
+ego_up <- enrichGO(gene = genes_symbol_up,
+                OrgDb = org.Hs.eg.db,
+                keyType = "SYMBOL",
+                ont = "BP",
+                pAdjustMethod = "BH",
+                pvalueCutoff = 0.05)
+
+ego_down <- enrichGO(gene = genes_symbol_down,
+                   OrgDb = org.Hs.eg.db,
+                   keyType = "SYMBOL",
+                   ont = "BP",
+                   pAdjustMethod = "BH",
+                   pvalueCutoff = 0.05)
+
+# Visualise GO enrichment analysis results
+dotplot(ego_up)
+dotplot(ego_down)
+
+# Extract target gene entrez ID (for KEGG enrichment compatibility)
+genes_entrez_up <- unique(target_results_up@data$target_entrez)
+genes_entrez_down <- unique(target_results_down@data$target_entrez)
+
+# Perform KEGG enrichment analysis
+ekegg_up <- enrichKEGG(genes_entrez_up)
+ekegg_down <- enrichKEGG(genes_entrez_down)
+
+# Visualise KEGG enrichment analysis results
+dotplot(ekegg_up)
+dotplot(ekegg_down)
+
+# Test code (not part of main script)
+length(genes_entrez_up)
+length(genes_entrez_down)
+length(target_results_up@data$target_entrez)
+dim(target_results_up@data)
+head(target_results_up@data)
+dose24_data$Symbol
+
+# [split gene list by upregulated and downregulated mirnas]
+# [Outstanding: 
+#              -  enrichPathway()]
+#              -  miRNA interaction network w/ Cytoscope]
+
